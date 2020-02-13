36329e553cabe8afa472ef1be594285464aba30a
Laurent Kvb <laurent@fullstackrebels.nl>
2020-02-13 10:06:46 +0100

    A Test commit

--- src/components/image/EpisodeImage.js
+++ src/components/image/EpisodeImage.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import './episodeImage.scss';
+import "./episodeImage.scss"
 
 export const EpisodeImage = ({ image, name }) => {
-  let imageCouldNotBeFound = 'ik wil hier een andere text hebbemn';
+  if (image && image.medium) {
+    return <img alt={name} src={image && image.medium ? image.medium : null} />;
+  }
 
-
-    return (
-        <div>
-            <p>{imageCouldNotBeFound}</p>
-            <p>{imageCouldNotBeFound}</p>
-            <p>{imageCouldNotBeFound}</p>
-            <p>{imageCouldNotBeFound}</p>
-            <img alt={name} src={image && image.medium ? image.medium : null} />
-        </div>
-    );
+  return (
+    <div className="episode-image__not-found">
+      <p>Image not found</p>
+    </div>
+  );
 };
 export default EpisodeImage;
