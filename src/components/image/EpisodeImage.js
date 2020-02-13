37c6308e17107475a245e4e9e1395604412a58cb
Laurent Kvb <laurent@fullstackrebels.nl>
2020-02-13 10:01:29 +0100

    A Test commit

--- src/components/image/EpisodeImage.js
+++ src/components/image/EpisodeImage.js
@@ -2,21 +2,17 @@ import React from 'react';
 import './episodeImage.scss';
 
 export const EpisodeImage = ({ image, name }) => {
-  let imageCouldNotBeFound = 'image could not be found ';
-  if (image && image.medium) {
-    imageCouldNotBeFound = 'Image is here ';
+  let imageCouldNotBeFound = 'ik wil hier een andere text hebbemn';
+
+
     return (
-      <div>
-        <p>{imageCouldNotBeFound}</p>
-        <img alt={name} src={image && image.medium ? image.medium : null} />
-      </div>
+        <div>
+            <p>{imageCouldNotBeFound}</p>
+            <p>{imageCouldNotBeFound}</p>
+            <p>{imageCouldNotBeFound}</p>
+            <p>{imageCouldNotBeFound}</p>
+            <img alt={name} src={image && image.medium ? image.medium : null} />
+        </div>
     );
-  } else {
-    return (
-      <div className="episode-image__not-found">
-        <p>{imageCouldNotBeFound}</p>
-      </div>
-    );
-  }
 };
 export default EpisodeImage;
